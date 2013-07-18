87fba7cb74f759913e0668a5bf92ee6fc5615660
Nginx <Nginx@gert.partitopirata.org>
2013-07-18 09:34:40 +0000

    nothing so special

---
+++ UpdateGithub.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+#
+# UpdateGithub.sh
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+
+git config --global user.name "mutek"
+git config --global user.email mutek@inventati.org
+
+echo "Aggiungo i cambiamenti al database locale"
+git add .
+echo "Inserisci una descrizione dei tuoi cambiamenti"
+read n
+echo "Eseguo la finalizzazione dei cambiamenti in locale"
+git commit -a -m "$n"
+
+echo "Invio i cambiamenti locali al deposito remoto..."
+git push origin master
+echo "ok"
